87b82244074dc2918001a36d905b7ef57e982bb2
pratamawijaya <set.mnemonix@gmail.com>
2016-08-26 23:14:40 +0700

    add some library for next development

--- app/src/main/java/com/pratamawijaya/panggilpeta/presentation/ui/login/LoginActivity.java
+++ app/src/main/java/com/pratamawijaya/panggilpeta/presentation/ui/login/LoginActivity.java
@@ -2,6 +2,9 @@ package com.pratamawijaya.panggilpeta.presentation.ui.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.pratamawijaya.panggilpeta.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.btn_login)void clickLogin(){
+    Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT).show();
   }
 }
